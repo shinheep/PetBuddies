d9bdfcba07436d0ceccc78817c498ae68f1d670d
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-11-03 14:40:23 -0700

    adding deployment files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
     <Router>
